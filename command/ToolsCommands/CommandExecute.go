ef194c766b93aa0f0b245e46d955ef528447296d
库玛 <2791528600@qq.com>
2026-05-30 01:11:32 +0800

    feat(miusers, cli): 实现完整用户管理系统与交互式admin-cli
    重构用户管理器模块，添加完善的错误处理流程，将原有的直接退出逻辑改为错误返回
    新增用户认证、密码/角色修改、用户删除、远程登录权限设置等核心方法
    扩展工具命令，新增chpwd、chmod、remove、remote命令与交互式admin-cli
    优化sys-init初始化流程，自动创建profiles目录并完善文件状态检查
    统一用户数据的加密、存储与加载逻辑，修复潜在权限问题

--- command/ToolsCommands/CommandExecute.go
+++ command/ToolsCommands/CommandExecute.go
@@ -1,36 +1,72 @@
 package toolscommands
 
 import (
+	"bufio"
+	"errors"
 	"fmt"
+	"io"
 	"misakadb/clilog"
 	"misakadb/miusers"
 	"misakadb/safe"
 	"os"
+	"strings"
 )
 
 func CommandExecute(command_all []string) {
 	main_command := command_all[0]
 	userManager := miusers.NewUserManager()
+	reader := bufio.NewReader(os.Stdin)
 
 	switch main_command {
 	case "sys-init":
-		_, user_err := os.Stat(miusers.UserFile)
-		if user_err == nil {
+		_, userErr := os.Stat(miusers.UserFile)
+		_, keyErr := os.Stat("./profiles/master.mikey")
+
+		if userErr == nil {
 			clilog.Error("数据库已经初始化了用户数据，无法再进行序列化！")
 			os.Exit(0)
 		}
-
-		_, password_err := os.Stat("./profiles/master.mikey")
-		if password_err == nil {
-			clilog.Error("数据库已经初始化了密钥数据，无法再进行序列化！")
+		if userErr != nil && !errors.Is(userErr, os.ErrNotExist) {
+			clilog.Error("无法检查用户文件状态:", userErr)
 			os.Exit(0)
 		}
-		// 密钥初始化
-		safe.InitPassword()
 
-		// 用户初始化
-		userManager.InitUser()
-		userload := userManager.LoadUserFile()
+		if keyErr != nil && !errors.Is(keyErr, os.ErrNotExist) {
+			clilog.Error("无法检查 AES 密钥文件状态:", keyErr)
+			os.Exit(0)
+		}
+
+		if errors.Is(userErr, os.ErrNotExist) && errors.Is(keyErr, os.ErrNotExist) {
+			if err := os.MkdirAll("./profiles", 0700); err != nil {
+				clilog.Error("创建 profiles 目录失败:", err)
+				os.Exit(0)
+			}
+			safe.InitPassword()
+		} else if errors.Is(userErr, os.ErrNotExist) && keyErr == nil {
+			clilog.Info("检测到已有 AES 密钥，继续复用该密钥初始化用户数据。")
+		} else if userErr == nil && errors.Is(keyErr, os.ErrNotExist) {
+			clilog.Error("检测到 user.dat 存在但 master.mikey 缺失，数据状态异常，请手动修复后再执行。")
+			os.Exit(0)
+		}
+
+		rootSecret, err := userManager.InitUser()
+		if err != nil {
+			clilog.Error("初始化用户失败:", err)
+			os.Exit(0)
+		}
+
+		userload, err := userManager.LoadUserFile()
+		if err != nil {
+			clilog.Error("读取初始化后的用户数据失败:", err)
+			os.Exit(0)
+		}
+
+		clilog.Success(
+			fmt.Sprintf(
+				"初始化用户信息表完毕, 这是 root 的密钥 %s。不包含前后符号，请立即执行：misaka-tools chpwd root",
+				rootSecret,
+			),
+		)
 		clilog.Info(fmt.Sprintf("一共初始化 %d 个用户.", len(userload)))
 
 	case "add-user":
@@ -40,9 +76,226 @@ func CommandExecute(command_all []string) {
 		}
 		username := command_all[1]
 		password := command_all[2]
-		all_user := userManager.AddUser(username, password)
-		clilog.Success(fmt.Sprintf("添加用户 %s 成功，当前还有 %d 个用户", username, len(all_user)))
+		allUser, err := userManager.AddUser(username, password)
+		if err != nil {
+			clilog.Error(err)
+			os.Exit(0)
+		}
+		clilog.Success(fmt.Sprintf("添加用户 %s 成功，当前还有 %d 个用户", username, len(allUser)))
+
+	case "change-password", "chpwd":
+		if len(command_all) != 2 {
+			clilog.Error("错误用法，正确用法misaka-tools chpwd 用户名")
+			os.Exit(0)
+		}
+		if err := requireRootAuth(userManager, reader); err != nil {
+			clilog.Error(err)
+			os.Exit(0)
+		}
+		newPassword, err := promptInput(reader, "请输入新的用户密码: ")
+		if err != nil {
+			clilog.Error("读取新密码失败:", err)
+			os.Exit(0)
+		}
+		if err := userManager.ChangePassword(command_all[1], newPassword); err != nil {
+			clilog.Error(err)
+			os.Exit(0)
+		}
+		clilog.Success(fmt.Sprintf("用户 %s 的密码修改成功", command_all[1]))
+
+	case "chmod":
+		if len(command_all) != 3 {
+			clilog.Error("错误用法，正确用法misaka-tools chmod 用户名 角色")
+			os.Exit(0)
+		}
+		if err := requireRootAuth(userManager, reader); err != nil {
+			clilog.Error(err)
+			os.Exit(0)
+		}
+		if err := userManager.ChangeRole(command_all[1], command_all[2]); err != nil {
+			clilog.Error(err)
+			os.Exit(0)
+		}
+		clilog.Success(fmt.Sprintf("用户 %s 的角色已修改为 %s", command_all[1], command_all[2]))
+
+	case "remove":
+		if len(command_all) != 2 {
+			clilog.Error("错误用法，正确用法misaka-tools remove 用户名")
+			os.Exit(0)
+		}
+		if err := requireRootAuth(userManager, reader); err != nil {
+			clilog.Error(err)
+			os.Exit(0)
+		}
+		if err := userManager.RemoveUser(command_all[1]); err != nil {
+			clilog.Error(err)
+			os.Exit(0)
+		}
+		clilog.Success(fmt.Sprintf("用户 %s 已删除", command_all[1]))
+
+	case "remote":
+		if len(command_all) != 3 {
+			clilog.Error("错误用法，正确用法misaka-tools remote 用户名 true|false")
+			os.Exit(0)
+		}
+		if err := requireRootAuth(userManager, reader); err != nil {
+			clilog.Error(err)
+			os.Exit(0)
+		}
+		flag, err := parseRemoteFlag(command_all[2])
+		if err != nil {
+			clilog.Error(err)
+			os.Exit(0)
+		}
+		if err := userManager.SetRemote(command_all[1], flag); err != nil {
+			clilog.Error(err)
+			os.Exit(0)
+		}
+		clilog.Success(fmt.Sprintf("用户 %s 的远程登录已设置为 %t", command_all[1], flag))
+
+	case "admin-cli":
+		if err := requireRootAuth(userManager, reader); err != nil {
+			clilog.Error(err)
+			os.Exit(0)
+		}
+		runAdminCLI(userManager, reader)
 	default:
 		clilog.Error("错误的命令, 你无法这么实现。")
 	}
+}
+
+func promptInput(reader *bufio.Reader, prompt string) (string, error) {
+	fmt.Print(prompt)
+	text, err := reader.ReadString('\n')
+	if err != nil && !errors.Is(err, io.EOF) {
+		return "", err
+	}
+	return strings.TrimSpace(text), nil
+}
+
+func requireRootAuth(userManager *miusers.UserManager, reader *bufio.Reader) error {
+	username, err := promptInput(reader, "请输入执行用户名: ")
+	if err != nil {
+		return err
+	}
+	password, err := promptInput(reader, "请输入执行用户密码: ")
+	if err != nil {
+		return err
+	}
+	if err := userManager.VerifyRole(username, "root"); err != nil {
+		return fmt.Errorf("root 权限鉴权失败: %w", err)
+	}
+	if err := userManager.VerifyPassword(username, password); err != nil {
+		return fmt.Errorf("用户 %s 鉴权失败: %w", username, err)
+	}
+	return nil
+}
+
+func parseRemoteFlag(flag string) (bool, error) {
+	switch strings.ToLower(strings.TrimSpace(flag)) {
+	case "1", "true", "on", "enable", "enabled", "yes":
+		return true, nil
+	case "0", "false", "off", "disable", "disabled", "no":
+		return false, nil
+	default:
+		return false, errors.New("remote 标记仅支持 true/false、on/off、1/0")
+	}
+}
+
+func runAdminCLI(userManager *miusers.UserManager, reader *bufio.Reader) {
+	clilog.Success("已进入 admin-cli，root 角色鉴权成功。输入 help 查看命令，输入 exit 退出。")
+
+	for {
+		commandLine, err := promptInput(reader, "misaka-admin> ")
+		if err != nil {
+			clilog.Error("读取命令失败:", err)
+			return
+		}
+		if commandLine == "" {
+			continue
+		}
+
+		args := strings.Fields(commandLine)
+		switch args[0] {
+		case "exit", "quit":
+			clilog.Info("admin-cli 已退出。")
+			return
+		case "help":
+			printAdminHelp()
+		case "change-password", "chpwd":
+			if len(args) != 2 {
+				clilog.Error("正确用法: chpwd <username>")
+				continue
+			}
+			newPassword, err := promptInput(reader, "请输入新的用户密码: ")
+			if err != nil {
+				clilog.Error("读取新密码失败:", err)
+				continue
+			}
+			if err := userManager.ChangePassword(args[1], newPassword); err != nil {
+				clilog.Error(err)
+				continue
+			}
+			clilog.Success(fmt.Sprintf("用户 %s 的密码修改成功", args[1]))
+		case "chmod":
+			if len(args) != 3 {
+				clilog.Error("正确用法: chmod <username> <role>")
+				continue
+			}
+			if err := userManager.ChangeRole(args[1], args[2]); err != nil {
+				clilog.Error(err)
+				continue
+			}
+			clilog.Success(fmt.Sprintf("用户 %s 的角色已修改为 %s", args[1], args[2]))
+		case "remove":
+			if len(args) != 2 {
+				clilog.Error("正确用法: remove <username>")
+				continue
+			}
+			if err := userManager.RemoveUser(args[1]); err != nil {
+				clilog.Error(err)
+				continue
+			}
+			clilog.Success(fmt.Sprintf("用户 %s 已删除", args[1]))
+		case "remote":
+			if len(args) != 3 {
+				clilog.Error("正确用法: remote <username> <flag>")
+				continue
+			}
+			flag, err := parseRemoteFlag(args[2])
+			if err != nil {
+				clilog.Error(err)
+				continue
+			}
+			if err := userManager.SetRemote(args[1], flag); err != nil {
+				clilog.Error(err)
+				continue
+			}
+			clilog.Success(fmt.Sprintf("用户 %s 的远程登录已设置为 %t", args[1], flag))
+		case "add-user":
+			if len(args) != 3 {
+				clilog.Error("正确用法: add-user <username> <password>")
+				continue
+			}
+			if _, err := userManager.AddUser(args[1], args[2]); err != nil {
+				clilog.Error(err)
+				continue
+			}
+			clilog.Success(fmt.Sprintf("用户 %s 添加成功", args[1]))
+		default:
+			clilog.Error("未知命令，输入 help 查看 admin-cli 支持的命令。")
+		}
+	}
+}
+
+func printAdminHelp() {
+	clilog.Info("可用命令:")
+	fmt.Println("  chpwd <username>")
+	fmt.Println("  change-password <username>")
+	fmt.Println("  chmod <username> <role>")
+	fmt.Println("  remove <username>")
+	fmt.Println("  remote <username> <flag>")
+	fmt.Println("  add-user <username> <password>")
+	fmt.Println("  help")
+	fmt.Println("  exit")
 }
--- miusers/userManager.go
+++ miusers/userManager.go
@@ -2,10 +2,11 @@ package miusers
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
-	"misakadb/clilog"
 	"misakadb/safe"
 	"os"
+	"strings"
 
 	"github.com/google/uuid"
 	"golang.org/x/crypto/bcrypt"
@@ -28,96 +29,231 @@ var (
 	UserFile = "./profiles/user.dat"
 )
 
-func (u *UserManager) InitUser() {
-
-	clilog.Info("正在初始化数据表中...")
+func (u *UserManager) InitUser() (string, error) {
 	random_password_no_salt := uuid.New().String()
-	empty, err_empty := safe.EncryptByte([]byte("{}"))
-	if err_empty != nil {
-		clilog.Error("加密数据失败！")
-		os.Exit(0)
-	}
-	err := os.WriteFile(UserFile, empty, 0600)
+	empty, err := safe.EncryptByte([]byte("{}"))
 	if err != nil {
-		clilog.Error("无法写入文件，请检查根目录中是否存在profiles文件夹。")
-		os.Exit(0)
+		return "", err
 	}
 
-	userJsonMap := u.AddUser("root", random_password_no_salt)
-	u.SaveUserFile(userJsonMap)
+	if err := os.MkdirAll("./profiles", 0700); err != nil {
+		return "", err
+	}
 
-	clilog.Success(
-		fmt.Sprintf(
-			"初始化用户信息表完毕, 这是root的密码 %s。不包含前后符号，请在第一时间进行修改，命令：misaka-tools chpwd root。",
-			random_password_no_salt,
-		),
-	)
+	err = os.WriteFile(UserFile, empty, 0600)
+	if err != nil {
+		return "", errors.New("无法写入用户文件，请检查 profiles 目录权限")
+	}
+
+	if _, err := u.AddUserWithRole("root", random_password_no_salt, "root"); err != nil {
+		return "", err
+	}
+
+	return random_password_no_salt, nil
 }
 
-func (u *UserManager) SaveUserFile(userMap map[string]UserJSON) {
-
+func (u *UserManager) SaveUserFile(userMap map[string]UserJSON) error {
 	jsonData, err := json.Marshal(userMap)
+	if err != nil {
+		return err
+	}
 
 	// 加密用户信息
 	cipherData, err := safe.EncryptByte(jsonData)
 	if err != nil {
-		clilog.Error("加密数据失败！")
-		os.Exit(0)
+		return err
 	}
 
 	err = os.WriteFile(UserFile, cipherData, 0600)
 	if err != nil {
-		clilog.Error("无法写入文件，请检查根目录中是否存在profiles文件夹。")
-		panic("system exit.")
+		return errors.New("无法写入用户文件，请检查 profiles 目录权限")
 	}
 
+	return nil
 }
 
-func (u *UserManager) LoadUserFile() map[string]UserJSON {
+func (u *UserManager) LoadUserFile() (map[string]UserJSON, error) {
 	cryData, err := os.ReadFile(UserFile)
 	if err != nil {
-		clilog.Error("无法读取原始的用户数据，请检查对应文件是否存在!")
-		os.Exit(0)
+		return nil, errors.New("无法读取用户数据，请检查 user.dat 是否存在")
 	}
 
 	plain, err := safe.DecryptByte(cryData)
 	if err != nil {
-		clilog.Error("无法解密的用户数据!")
-		os.Exit(0)
+		return nil, errors.New("无法解密用户数据，请检查 AES 密钥是否匹配")
 	}
 
 	userJson := make(map[string]UserJSON)
 
 	err = json.Unmarshal(plain, &userJson)
 	if err != nil {
-		clilog.Error("无法解密的用户数据!")
-		os.Exit(0)
+		return nil, errors.New("用户数据格式错误")
 	}
-	return userJson
+
+	return userJson, nil
 }
 
-func (u *UserManager) AddUser(username string, password string) map[string]UserJSON {
-	password_hash, err := bcrypt.GenerateFromPassword(
+func (u *UserManager) AddUser(username string, password string) (map[string]UserJSON, error) {
+	return u.AddUserWithRole(username, password, "default")
+}
+
+func (u *UserManager) AddUserWithRole(username string, password string, role string) (map[string]UserJSON, error) {
+	username = strings.TrimSpace(username)
+	password = strings.TrimSpace(password)
+	role = strings.TrimSpace(role)
+
+	if username == "" {
+		return nil, errors.New("用户名不能为空")
+	}
+	if password == "" {
+		return nil, errors.New("密码不能为空")
+	}
+	if role == "" {
+		return nil, errors.New("角色不能为空")
+	}
+
+	passwordHash, err := bcrypt.GenerateFromPassword(
 		[]byte(password),
 		bcrypt.DefaultCost,
 	)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	userJSON := UserJSON{
-		Password: string(password_hash[:]),
-		Role:     "default",
+		Password: string(passwordHash),
+		Role:     role,
 		Remote:   true,
 	}
 
-	var user map[string]UserJSON
-	user = u.LoadUserFile()
+	user, err := u.LoadUserFile()
+	if err != nil {
+		return nil, err
+	}
 	if _, ok := user[username]; ok {
-		clilog.Error(fmt.Sprintf("用户 %s 已存在", username))
-		os.Exit(0)
+		return nil, fmt.Errorf("用户 %s 已存在", username)
 	}
 
 	user[username] = userJSON
-	u.SaveUserFile(user)
-	return user
+	if err := u.SaveUserFile(user); err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (u *UserManager) VerifyPassword(username string, password string) error {
+	userMap, err := u.LoadUserFile()
+	if err != nil {
+		return err
+	}
+
+	user, ok := userMap[username]
+	if !ok {
+		return fmt.Errorf("用户 %s 不存在", username)
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+		return errors.New("密码错误")
+	}
+
+	return nil
+}
+
+func (u *UserManager) VerifyRole(username string, role string) error {
+	userMap, err := u.LoadUserFile()
+	if err != nil {
+		return err
+	}
+
+	user, ok := userMap[username]
+	if !ok {
+		return fmt.Errorf("用户 %s 不存在", username)
+	}
+
+	if user.Role != strings.TrimSpace(role) {
+		return fmt.Errorf("用户 %s 没有 %s 权限", username, role)
+	}
+
+	return nil
+}
+
+func (u *UserManager) ChangePassword(username string, password string) error {
+	password = strings.TrimSpace(password)
+	if password == "" {
+		return errors.New("新密码不能为空")
+	}
+
+	userMap, err := u.LoadUserFile()
+	if err != nil {
+		return err
+	}
+
+	user, ok := userMap[username]
+	if !ok {
+		return fmt.Errorf("用户 %s 不存在", username)
+	}
+
+	passwordHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+
+	user.Password = string(passwordHash)
+	userMap[username] = user
+	return u.SaveUserFile(userMap)
+}
+
+func (u *UserManager) ChangeRole(username string, role string) error {
+	role = strings.TrimSpace(role)
+	if role == "" {
+		return errors.New("角色不能为空")
+	}
+
+	userMap, err := u.LoadUserFile()
+	if err != nil {
+		return err
+	}
+
+	user, ok := userMap[username]
+	if !ok {
+		return fmt.Errorf("用户 %s 不存在", username)
+	}
+
+	user.Role = role
+	userMap[username] = user
+	return u.SaveUserFile(userMap)
+}
+
+func (u *UserManager) SetRemote(username string, remote bool) error {
+	userMap, err := u.LoadUserFile()
+	if err != nil {
+		return err
+	}
+
+	user, ok := userMap[username]
+	if !ok {
+		return fmt.Errorf("用户 %s 不存在", username)
+	}
+
+	user.Remote = remote
+	userMap[username] = user
+	return u.SaveUserFile(userMap)
+}
+
+func (u *UserManager) RemoveUser(username string) error {
+	if strings.TrimSpace(username) == "root" {
+		return errors.New("root 用户不允许删除")
+	}
+
+	userMap, err := u.LoadUserFile()
+	if err != nil {
+		return err
+	}
+
+	if _, ok := userMap[username]; !ok {
+		return fmt.Errorf("用户 %s 不存在", username)
+	}
+
+	delete(userMap, username)
+	return u.SaveUserFile(userMap)
 }
