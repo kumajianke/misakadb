9e46d23e3d8b135e234d2285b9a4f2a70247364e
hello_kuma <2791528600@qq.com>
2026-06-05 14:10:10 +0800

    解决了竞态问题

--- lock/global_lock/GlobalLock.go
+++ lock/global_lock/GlobalLock.go
@@ -9,6 +9,9 @@ import (
 	"github.com/puzpuzpuz/xsync/v3"
 )
 
+var once sync.Once
+var globalLockPools *GlobalLocksPool
+
 // 全局锁池
 type GlobalLocks struct {
 	RefCounter atomic.Int32
@@ -16,5 +19,35 @@ type GlobalLocks struct {
 }
 
 type GlobalLocksPool struct {
-	LocksPool xsync.MapOf[string, *GlobalLocks]
+	YoungPool xsync.MapOf[string, *GlobalLocks] // 当前热门的锁
+	OldPool   xsync.MapOf[string, *GlobalLocks] // 分代锁；即将淘汰的锁用于GC
+}
+
+func GetGlobalLockPool() *GlobalLocksPool {
+	once.Do(func() {
+		globalLockPools = &GlobalLocksPool{}
+	})
+	return globalLockPools
+}
+
+/**
+* 获取一个锁，如果锁是第一次创建，自动注册到全局锁池
+ */
+func GetOrStoreGlobalLock(lock_name string, lockit bool) (*GlobalLocks, func()) {
+	pools := GetGlobalLockPool()
+
+	// 尝试直接在新池中获取锁
+	lock, cache_hit := pools.YoungPool.Load(lock_name)
+	if !cache_hit {
+		// 如果缓存没有命中 查看是否旧池中有这个锁 有的话获取对应的旧值并给他删掉
+		lock, cache_hit = pools.OldPool.Load(lock_name)
+		if cache_hit {
+			pools.YoungPool.LoadOrStore(lock_name, lock) // 升级锁
+			pools.OldPool.Delete(lock_name)
+		} else {
+			pools.YoungPool.LoadOrStore(lock_name, &GlobalLocks{}) // 新旧池都没有 就新建一个
+		}
+	}
+	//TODO
+	return nil, nil
 }
