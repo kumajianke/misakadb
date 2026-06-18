ce917db01a7e36b931492e8f1754df84dd3b5f6b
hello_kuma <2791528600@qq.com>
2026-06-18 09:02:54 +0800

    功能(atomicWorkCenter): 新增AddTask和GetTask任务处理方法
    新增任务添加与获取方法，AddTask支持重复ID重试避免冲突，通过时间戳加随机数生成MD5唯一任务键，使用原子操作保证线程安全

--- atomic/atomicWorkCenter/AtomicWorkCenter.go
+++ atomic/atomicWorkCenter/AtomicWorkCenter.go
@@ -1,8 +1,17 @@
 package atomic_work_center
 
 import (
+	"crypto/md5"
+	"math/rand"
+	"strconv"
+	"time"
+
 	"github.com/puzpuzpuz/xsync/v3"
 )
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 var AtomicWorkCenterInstance *AtomicWorkCenter
 
@@ -18,4 +27,26 @@ func NewAtomicWorkCenter() *AtomicWorkCenter {
 		}
 	}
 	return AtomicWorkCenterInstance
+}
+
+func (this *AtomicWorkCenter) AddTask(task *Task, retry int) bool {
+	now := time.Now().Format("2006-1-2 15:04:05") + strconv.Itoa(rand.Intn(30000)+1)
+	md5Hash := md5.Sum([]byte(now))
+	_, loaded := this.TasksMap.LoadOrStore(string(md5Hash[:]), task)
+	if loaded {
+		// 如果Map中存在
+		if retry <= 0 {
+			return false
+		}
+		return this.AddTask(task, retry-1)
+	}
+	return true
+}
+
+func (this *AtomicWorkCenter) GetTask(taskId string) *Task {
+
+	if task, ok := this.TasksMap.Load(taskId); ok {
+		return task
+	}
+	return nil
 }
