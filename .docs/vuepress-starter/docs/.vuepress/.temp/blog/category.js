0a47055fc8b99919762940cedbacc8405b716045
hello_kuma <2791528600@qq.com>
2026-06-29 12:40:40 +0800

    更新文档

--- .docs/vuepress-starter/docs/.vuepress/.temp/blog/category.js
+++ .docs/vuepress-starter/docs/.vuepress/.temp/blog/category.js
@@ -1,13 +1,2 @@
 export const categoriesMap = JSON.parse("{\"category\":{\"/\":{\"path\":\"/category/\",\"map\":{\"内部实现\":{\"path\":\"/category/%E5%86%85%E9%83%A8%E5%AE%9E%E7%8E%B0/\",\"indexes\":[0,1]}}}},\"tag\":{\"/\":{\"path\":\"/tag/\",\"map\":{\"全局锁池\":{\"path\":\"/tag/%E5%85%A8%E5%B1%80%E9%94%81%E6%B1%A0/\",\"indexes\":[1]},\"原子任务\":{\"path\":\"/tag/%E5%8E%9F%E5%AD%90%E4%BB%BB%E5%8A%A1/\",\"indexes\":[0]}}}}}");
 
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept();
-  if (__VUE_HMR_RUNTIME__.updateBlogCategory)
-    __VUE_HMR_RUNTIME__.updateBlogCategory(categoriesMap);
-}
-
-if (import.meta.hot)
-  import.meta.hot.accept(({ categoriesMap }) => {
-    __VUE_HMR_RUNTIME__.updateBlogCategory(categoriesMap);
-  });
-
